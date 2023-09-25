a225ecd73685d8453efe377850a8f74b54ca723c
Rzvone <bobonea.razvan.ctin@gmail.com>
2023-09-25 10:58:29 +0300

    Initial commit

---
+++ backend/src/test/java/com/codecool/colorup/ColorupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.colorup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
