8d27f9385979144d4d946f3848067e0557b510bc
Ionut2707 <ionut.dima2707@gmail.com>
2023-09-28 13:54:38 +0300

    register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import About from './Components/About';
+import Error from './Components/Error';
+import NavBar from './Components/NavBar';
+
+import Register from './Pages/Register';
 
 function App() {
   
   return (
-  
-    <h1 className='text-3xl font-bld underline'>
-      Hello World !
-    </h1>
+    <div>
+      <NavBar/>
+      <Routes> 
+        <Route path='/about' element={<About/>} /> 
+        <Route path='/error' element={<Error />} />
+        <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </div>
   );
 }
 
