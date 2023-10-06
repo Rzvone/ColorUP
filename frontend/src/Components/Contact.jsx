eedd6bb2e6dddbb3666a7dace68e8cb75279d624
Ionut2707 <ionut.dima2707@gmail.com>
2023-10-06 18:06:23 +0300

    google address

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -4,7 +4,7 @@ import emailjs from "@emailjs/browser";
 export const Contact = () => {
   const form = useRef();
 
-  const sendEmail = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     //ADD  your correct email address
     //Use the EmailJS website (www.emailjs.com)
@@ -32,15 +32,44 @@ export const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section>
+      <div>
+        <div className="contact-container">
+          <h1>Contact details</h1>
+          <p>E-mail: www.example.com</p>
+          <p>Tel:+407xxxxxxxx</p>
+          <p>Address: Str.Pantofului, Nr.Cartofului</p>
+        </div>
+        <div class="form-container">
+          <h3>Message Us</h3>
+          <form ref={form} onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" placeholder="Type your message..." />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+      <div className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.723297912627!2d25.994905076831998!3d44.928962271070276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b249a220af0053%3A0xdaa335f87fb32130!2sAleea%20Prislop%207%2C%20Ploie%C8%99ti%20107592!5e0!3m2!1sro!2sro!4v1696600372815!5m2!1sro!2sro"
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            border: "1px solid black",
+          }}
+          width="600"
+          height="450"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </section>
   );
 };
 
